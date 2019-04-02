8e226f004ea0624eac766c5ce1a41451cb205262
waithiageni <mwangiwaithiageni33@gmail.com>
2019-04-03 00:21:30 +0300

    added calculator

--- calculator.py
+++ calculator.py
@@ -2,7 +2,29 @@ def add(a,b):
 	c=a+b
 	print(c)
 
-def subtract(c,d):
-	e=d-c
-	print(e)
-	
+def subtract(a,b):
+	c=a-b
+	print(c)
+
+def multiply(a,b):
+	c=a*b
+	print(c)
+
+def division(a,b):
+	c=a/b
+	print(c)
+def modulus(a,b):
+	c=a%b
+	print(c)
+
+def percentage(a,b):
+	c=a/b*100
+	print(c)
+
+def whole_number_division(a,b):
+	c=a//b
+	print(c)
+
+def exponent(a,b):
+	c=a**b
+	print(c)
